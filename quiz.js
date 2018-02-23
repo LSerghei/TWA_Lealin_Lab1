15e7b64aff3184c11391bbdefb81190fd43be1d9
Serghei Lealin <serghei.lealin@gmail.com>
2018-02-23 14:42:59 +0200

    added new questions, user input for count of questions

--- quiz.js
+++ quiz.js
@@ -4,13 +4,21 @@ var jsonQuestions = [{questionText : "What was the first console video game that
 					{questionText : "Created in 2009, what was the first decentralized cryptocurrency? </br> a) Ethereum </br> b) Ripple </br> c) Bitcoin", answerChar : "c"},
 					{questionText : "The USB in 'USB Cable' stands for what? </br> a) Utility Service Bind </br> b) Uninterrupted Service Bus </br> c) Universal Serial Bus </br> d) Uniform Sync Bank", answerChar : "c"},
 					{questionText : "Which company did Steve Jobs ask to build OSX-based Laptops? </br> a) Sony </br> b) Hewlett-Packard </br> c) Dell </br> d) IBM", answerChar : "a"},
+					{questionText : "Which computer technology derives its name from Greek mythology? </br> a) Daemons </br> b) Caches </br> c) NAND Gates </br> d) Processors", answerChar : "a"},
+					{questionText : "The largest fresh water lake in the world is located in? </br> a) Brazil </br> b) Russia </br> c) The United States </br> d) China", answerChar : "b"},
+					{questionText : "Canada and which of these U.S. States have very similar population sizes? </br> a) New York </br> b) California </br> c) Florida </br> d) Texas", answerChar : "b"},
+					{questionText : "The opposite of Albinism is called? </br> a) Velvetinism </br> b) Alopecia X </br> c) Vitiligo </br> d) Melanism", answerChar : "d"},
+					{questionText : "Who produced the world's first commercial 1TB Hard Drive? </br> a) Western Digital </br> b) Hitachi </br> c) Maxtor </br> d) Seagate", answerChar : "b"},
+					{questionText : "The fattiest organ of the human body is the? </br> a) Pancreas </br> b) Heart </br> c) Brain </br> d) Liver", answerChar : "c"},
 					{questionText : "After whom is the Linux operating system named? </br> a) Lineaus Henderson </br> b) Leonardo da Vinci </br> c) Linus Pauling </br> d) Linus Torvalds", answerChar : "d"}];
 
 var QUESTIONS_COUNT = jsonQuestions.length;
-var QUESTIONS_SHOW_COUNT = 3;
+var QUESTIONS_SHOW_COUNT = 5;
 
 var questionIDs = [];
 var hQuestions = [], pQuestions = [], iAnswers = [];
+var sQuestionCount;
+var bAnswer;
 
 var startTime, endTime;
 
@@ -27,15 +35,14 @@ for (i = 0; i < QUESTIONS_SHOW_COUNT; i++){
 
 //Functions
 function checkAnswer(){
-	var vAnswers = [];
 	var countAnswer = 0;
 	var totalSeconds;
 	
 	for (i = 0; i < QUESTIONS_SHOW_COUNT; i++){
 		//Remove loose spaces and set to lower case like in answers
-		vAnswers.push(document.getElementById("a" + questionIDs[i]).value.trim().toLowerCase());
+		jsonQuestions[questionIDs[i]].answerFromUser = document.getElementById("a" + questionIDs[i]).value.trim().toLowerCase();
 		
-		if (vAnswers[i] == jsonQuestions[questionIDs[i]].answerChar) {
+		if (jsonQuestions[questionIDs[i]].answerFromUser === jsonQuestions[questionIDs[i]].answerChar) {
 			countAnswer++;
 		} 
 	}
@@ -47,7 +54,28 @@ function checkAnswer(){
 	alert("You answered " + countAnswer + " of " + QUESTIONS_SHOW_COUNT + " right!\n" + "It took you " + totalSeconds + " seconds.");
 }
 
+function createQuestionsInput(){
+	sQuestionCount = document.createElement("SELECT");
+	
+	for (i = 0; i < QUESTIONS_COUNT; i++){
+		var opt = document.createElement("OPTION");
+		opt.value = i+1;
+		opt.innerHTML = i+1;
+		sQuestionCount.appendChild(opt);
+	}		
+	
+	document.body.appendChild(sQuestionCount);
+}
+
 function init(){
+	//Hide question counter
+	document.getElementById("pCount").style.display = "none";
+	document.getElementById("bStart").style.display = "none";
+	sQuestionCount.style.display = "none";
+	
+	QUESTIONS_SHOW_COUNT = sQuestionCount.options[sQuestionCount.selectedIndex].value;
+	
+	//Create H4 and P elements for questions' texts
 	for (i = 0; i < QUESTIONS_SHOW_COUNT; i++){
 		hQuestions.push(document.createElement("H4"));
 		hQuestions[i].innerHTML = "Question " + (i+1);
@@ -64,6 +92,16 @@ function init(){
 		
 		document.body.appendChild(iAnswers[i]);
 	}
+	
+	//Create two breaks for Answer button
+	document.body.appendChild(document.createElement("BR"));
+	document.body.appendChild(document.createElement("BR"));
+	
+	//Create Answer button
+	bAnswer = document.createElement("BUTTON");
+	bAnswer.setAttribute("onclick", "checkAnswer()");
+	bAnswer.innerHTML = "Answer";
+	document.body.appendChild(bAnswer);
 	
 	startTime = new Date();
 }
